25414e1ac10ae4b23df609af7f61fb301ecd59a9
John Doe <cormil@gmail.com>
2017-08-06 14:36:47 -0500

    setup apiRoutes.js

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -15,12 +15,12 @@ module.exports = function(app) {
   // In each of the below cases the user is shown an HTML page of content
   // ---------------------------------------------------------------------------
 
-  app.get("survey", function(req, res) {
-    res.sendFile(path.join(__dirname, "./app/public/survey.html"));
+  app.get("/survey", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/survey.html"));
   });
 
   // If no matching route is found default to home
   app.use(function(req, res) {
-    res.sendFile(path.join(__dirname, "./app/public/home.html"));
+    res.sendFile(path.join(__dirname, "../public/home.html"));
   });
 };
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 var path = require("path");
+
 var app = express();
 
 var PORT = process.env.PORT || 8080;
@@ -10,7 +11,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
 app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 
-require("./app/routing/apiRoutes")(app);
+// require("./app/routing/apiRoutes")(app);
 require("./app/routing/htmlRoutes")(app);
 
 app.listen(PORT, function() {
